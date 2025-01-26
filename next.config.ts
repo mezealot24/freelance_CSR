7271768660a1e95a58576791922679a7420d2d31
mezealot24 <wiriya.on@gmail.com>
2025-01-26 20:05:00 +0700

    disable ESLint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ปิด ESLint ระหว่างการ build (ไม่แนะนำสำหรับโปรดักชัน)
+  },
   /* config options here */
 };
 
